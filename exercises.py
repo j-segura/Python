6fd2fd155cae329a7b0d4b1d8be7c621823dec1a
j-segura <juanda.segura2005@gmail.com>
2023-06-05 11:13:14 -0500

    second commit

---
+++ exercises.py
@@ -0,0 +1,19 @@
+# TALLER 1 PYTHON
+# AUTOR: JUAN DAVID SEGURA ZABALA
+# FECHA: 05/06/1013
+
+from datetime import date
+
+hoy = date.today()
+print("Hoy es el dia: ", hoy)
+n1 = int(input("digite el primer numero: "))
+n2 = int(input("digite el segundo numero: "))
+suma = n1 + n2
+resta = n1 - n2
+producto = n1 * n2
+division = n1 / n2
+print("la suma es: ", suma)
+print("la resta es: ", resta)
+print("el producto es: ", producto)
+print("la division es: ", division)
+print('FIN')
